88bbf03ab51cb8e350b09e5133d7132d1b16a1a5
Douglas Lins <douglas.lins@ifood.com.br>
2021-07-05 23:06:50 -0300

    commit inicial

---
+++ src/main/java/service/BonusService.java
@@ -0,0 +1,17 @@
+package service;
+
+import model.Funcionario;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class BonusService {
+
+    public BigDecimal calcularBonus(Funcionario funcionario) {
+        BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+        if (valor.compareTo(new BigDecimal("1000")) > 0) {
+            valor = BigDecimal.ZERO;
+        }
+        return valor;
+    }
+}

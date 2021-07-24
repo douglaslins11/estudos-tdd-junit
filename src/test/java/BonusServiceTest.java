22ee28f23cdd08bd219a35000abc87d36b93d21d
Douglas Lins <douglas_lins11@hotmail.com>
2021-07-24 18:07:56 -0300

    refatoração nas classes de teste, removendo linhas duplicadas

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,4 +1,5 @@
 import model.Funcionario;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.BonusService;
 
@@ -10,26 +11,30 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BonusServiceTest {
 
+    private BonusService bonusService;
+
+    @BeforeEach
+    public void setup(){
+        this.bonusService = new BonusService();
+    }
+
     @Test
     public void deveRetornarExceçãoParaFuncionarioComSalarioMuitoAlto(){
-        BonusService service = new BonusService();
 
         assertThrows(IllegalArgumentException.class,
-                () -> service.calcularBonus(new Funcionario("Douglas", LocalDate.now(), new BigDecimal("15000"))));
+                () -> bonusService.calcularBonus(new Funcionario("Douglas", LocalDate.now(), new BigDecimal("15000"))));
     }
 
     @Test
     public void bonusDeveriaSer10PorCentoDoSalario(){
-        BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Douglas", LocalDate.now(), new BigDecimal("5000")));
+        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Douglas", LocalDate.now(), new BigDecimal("5000")));
 
         assertEquals(new BigDecimal("500.00"), bonus);
     }
 
     @Test
     public void bonusDeveriaSer10PorCentoParaSalarioDeExatamente10000(){
-        BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Douglas", LocalDate.now(), new BigDecimal("10000")));
+        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Douglas", LocalDate.now(), new BigDecimal("10000")));
 
         assertEquals(new BigDecimal("1000.00"), bonus);
     }
--- src/test/java/ReajusteServiceTest.java
+++ src/test/java/ReajusteServiceTest.java
@@ -1,5 +1,6 @@
 import model.Desempenho;
 import model.Funcionario;
+import org.junit.jupiter.api.BeforeEach;
 import service.ReajusteService;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +11,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReajusteServiceTest {
 
+    private Funcionario funcionario;
+    private ReajusteService reajusteService;
+
+    @BeforeEach
+    public void setup(){
+        this.funcionario = new Funcionario("Douglas", LocalDate.now(), new BigDecimal("1000"));
+        this.reajusteService = new ReajusteService();
+    }
+
     @Test
     public void reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADESEJAR (){
-        ReajusteService reajusteService = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Douglas", LocalDate.now(), new BigDecimal("1000"));
         reajusteService.concederReajuste(funcionario, Desempenho.A_DESEJAR);
 
         assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
@@ -21,8 +29,6 @@ public class ReajusteServiceTest {
 
     @Test
     public void reajusteDeveriaSerDeQuinzePorCentoQuandoDesempenhoForBOM (){
-        ReajusteService reajusteService = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Douglas", LocalDate.now(), new BigDecimal("1000"));
         reajusteService.concederReajuste(funcionario, Desempenho.BOM);
 
         assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
@@ -30,8 +36,6 @@ public class ReajusteServiceTest {
 
     @Test
     public void reajusteDeveriaSerDeVintePorCentoQuandoDesempenhoForOTIMO (){
-        ReajusteService reajusteService = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Douglas", LocalDate.now(), new BigDecimal("1000"));
         reajusteService.concederReajuste(funcionario, Desempenho.OTIMO);
 
         assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());

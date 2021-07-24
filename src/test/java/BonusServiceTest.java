450f10c67a448d279a16f760d620d6d1b546c79f
Douglas Lins <douglas_lins11@hotmail.com>
2021-07-24 17:30:49 -0300

    ajustando o teste para validar a exception retornada

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -6,15 +6,16 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BonusServiceTest {
 
     @Test
-    public void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto(){
+    public void deveRetornarExceçãoParaFuncionarioComSalarioMuitoAlto(){
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Douglas", LocalDate.now(), new BigDecimal("15000")));
 
-        assertEquals(new BigDecimal("0.00"), bonus);
+        assertThrows(IllegalArgumentException.class,
+                () -> service.calcularBonus(new Funcionario("Douglas", LocalDate.now(), new BigDecimal("15000"))));
     }
 
     @Test
